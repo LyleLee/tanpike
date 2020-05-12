b86c784d276593303346fb0eae2b975da02454ed
tanpike <tannerpike@protonmail.com>
2020-05-11 19:08:38 -0500

    new test

--- sphinxtst/source/conf.py
+++ sphinxtst/source/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'sphinxtest'
-copyright = '2020, scoronam'
-author = 'scoronam'
+copyright = '2020, tanpike'
+author = 'tanpike'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -167,7 +167,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_sttic']
 html_css_files = [
     'css/custom.css',
 ]
